64db9c6e1f9557dd25c0f3632166408ee580eee8
rayu <t108840810@gmail.com>
2019-05-15 15:05:30 +0800

    新增index.ts匯出檔案

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import JKoPaySDK from "./JKoPaySDK";
+import {EntryOptions, Product, EntryResponse} from "./interface/entry.interface";
+import {RefundResponse} from "./interface/refund.interface";
+import {InquiryResponse} from "./interface/inquiry.interface";
+
+export {JKoPaySDK, EntryOptions, Product, EntryResponse, RefundResponse, InquiryResponse};
+export default JKoPaySDK;
